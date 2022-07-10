55bdc88c27f29399a2ead2d5de8d92deb0980199
Glorious <gloriouselijah125@gmail.com>
2022-07-10 16:27:49 +0000

    implement merge sort for normal lists

---
+++ mergeSort.js
@@ -0,0 +1,77 @@
+const mergeSort = (list = [], rev = false) => {
+    if (list.length <= 1) {
+        return list;
+    }
+
+    let { leftHalf, rightHalf } = divide(list);
+    let left = mergeSort(leftHalf, rev);
+    let right = mergeSort(rightHalf, rev);
+
+    return sort(left, right, rev);
+};
+
+const divide = (list) => {
+    let mid = Math.floor(list.length / 2);
+    let leftHalf = list.slice(0, mid);
+    let rightHalf = list.slice(mid);
+
+    return { leftHalf, rightHalf };
+};
+
+const sort = (arg1, arg2, rev) => {
+    let newList = [];
+    let i = 0;
+    let j = 0;
+
+    if (rev) {
+        while (i < arg1.length && j < arg2.length) {
+            if (arg1[i] > arg2[j]) {
+                newList.push(arg1[i]);
+                i += 1;
+            } else {
+                newList.push(arg2[j]);
+                j += 1;
+            }
+        }
+        while (i < arg1.length) {
+            newList.push(arg1[i]);
+            i += 1;
+        }
+        while (j < arg2.length) {
+            newList.push(arg2[j]);
+            j += 1;
+        }
+
+        return newList;
+    } else {
+        while (i < arg1.length && j < arg2.length) {
+            if (arg1[i] < arg2[j]) {
+                newList.push(arg1[i]);
+                i += 1;
+            } else {
+                newList.push(arg2[j]);
+                j += 1;
+            }
+        }
+        while (i < arg1.length) {
+            newList.push(arg1[i]);
+            i += 1;
+        }
+        while (j < arg2.length) {
+            newList.push(arg2[j]);
+            j += 1;
+        }
+
+        return newList;
+    }
+};
+
+d = mergeSort(
+    [
+        45, 22, 243, 234, 3, 4, 23, 4, 36, 234, 2, 45, 32, 3, 32, 324, 2, 564,
+        4, 354, 3, 23,
+    ],
+    true
+);
+
+console.log(d);
---
+++ merge_sort.py
@@ -0,0 +1,71 @@
+def merge_sort(data, rev = False):
+    '''
+        Takes an unsorted data argument and returns a new sorted list. Returns list in ascending order if rev is False and vice versa
+    '''
+
+    if len(data) <= 1:
+        return data
+
+    #divide function returns a splitted list
+    left_half, right_half = divide(data)
+    #recursively call merge sort on both halves till only a single element or empty array remains
+    left = merge_sort(left_half, rev)
+    right = merge_sort(right_half, rev)
+
+    return merge(left, right, rev)
+
+def divide(data):
+    '''
+        Takes a list argument and divides it into two different list
+    '''
+    #find the midpoint of the list supplied 
+    mid = len(data) // 2
+    left_half = data[:mid]
+    right_half = data[mid:]
+
+    return left_half, right_half
+
+def merge(arg1, arg2, rev):
+    '''
+        Sorts arguments supplied in ascending order when the rev(reverse) argument is False and descending order when rev is True
+        and returns a new sorted list
+    '''
+    new_list = []
+    i = 0
+    j = 0
+
+    if rev:
+        while i < len(arg1) and j < len(arg2):
+            if arg1[i] > arg2[j]:
+                new_list.append(arg1[i])
+                i += 1
+            else:
+                new_list.append(arg2[j])
+                j += 1
+        while i < len(arg1):
+            new_list.append(arg1[i])
+            i += 1
+        while j < len(arg2):
+            new_list.append(arg2[j])
+            j += 1
+
+        return new_list
+        
+    else:
+        while i < len(arg1) and j < len(arg2):
+            if arg1[i] < arg2[j]:
+                new_list.append(arg1[i])
+                i += 1
+            else:
+                new_list.append(arg2[j])
+                j += 1
+        while i < len(arg1):
+            new_list.append(arg1[i])
+            i += 1
+        while j < len(arg2):
+            new_list.append(arg2[j])
+            j += 1
+        return new_list
+
+d = merge_sort([45,22,243,234,3,4,23,4,36,234,2,45,32,3,32,324,2,564,4,354,3,23], False)
+print(d) 
